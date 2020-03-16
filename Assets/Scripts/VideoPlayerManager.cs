74579553da642a1da68247173f9774774a7590f3
Mer-ui <57984661+Mer-ui@users.noreply.github.com>
2020-03-16 19:35:02 +0100

    Added logo and switching to game at end logo

---
+++ Assets/Scripts/VideoPlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoPlayerManager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    bool started;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Console.WriteLine("work");
+        if (videoPlayer.isPlaying)
+        {
+            started = true;
+        }
+
+        if(started && !videoPlayer.isPlaying)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
