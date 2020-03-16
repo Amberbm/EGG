d47b918786b55058b9a6e1e7a76959065dd95737
Mer-ui <57984661+Mer-ui@users.noreply.github.com>
2020-03-16 16:49:05 +0100

    Changes image depending on stage!

---
+++ Assets/Scripts/Stagemanager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stagemanager : MonoBehaviour
+{
+    public bool stage1, stage2;
+    public GameObject image1;
+    public GameObject image2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stage1 = true;
+        stage2 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MoneyManager.amount >= 20)
+        {
+            stage1 = false;
+            stage2 = true;
+        }
+        if(stage1)
+        {
+            image1.SetActive(true);
+            image2.SetActive(false);
+        }
+        else if(stage2)
+        {
+            image1.SetActive(false);
+            image2.SetActive(true);
+        }
+    }
+}
