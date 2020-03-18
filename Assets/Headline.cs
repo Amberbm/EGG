cb49a2f8b79a7a966b0bb4bba0bd360767bf97b2
Amber <ambertje14@icloud.com>
2020-03-18 18:34:30 +0100

    tags

---
+++ Assets/Headline.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Headline : MonoBehaviour
+{
+    public UnityEvent Add { get { if (add == null) add = new UnityEvent(); return add; } }
+    [SerializeField] private UnityEvent add;
+
+    public string title;
+    public string tag;
+    public float factor;
+
+    bool gameJustStarted = true; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameJustStarted)
+        {
+            Add.Invoke();
+            gameJustStarted = false;
+        }
+    }
+    
+}
---
+++ Assets/HeadlineManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class HeadlineManager : MonoBehaviour
+{
+    public List<Headline> headlines;
+    public List<Headline> currentHeadlines;
+    public Text headlineList;
+    public UnityEvent Show { get { if (show == null) show = new UnityEvent(); return show; } }
+    [SerializeField] private UnityEvent show;
+
+    public Text text;
+
+    void Start()
+    {
+        
+    }
+
+    public void AddHeadline(Headline headline)
+    {
+        headlines.Add(headline);
+    }
+
+    public void ShowHeadline()
+    {
+        
+      //  for (int i = 0; i < 3; i++)
+      //  {
+            int random = Random.Range(0, headlines.Count - 1);
+            Headline headline;
+            headline = headlines[random] ;
+            headlines.RemoveAt(random);
+            text.text = headline.title;
+            PropertyManager.tags.Find(x => x.tagName == headline.tag).InfluenceFactor *= headline.factor;
+            currentHeadlines.Add(headline);
+            Show.Invoke();
+        headlineList.text = headlineList.text + "\""+ headline.title + "\" \n";
+        //  }
+
+    }
+}
